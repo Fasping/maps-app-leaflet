93bf828f6c01afccf14c8bb5e914cfa6fd42d575
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-28 01:02:00 +0200

    Add markers

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 function App() {
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Hello im a pop up 1",
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "Hello im a pop up 2",
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "Hello im a pop up 3",
+    },
+  ];
+
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.opentreetmap.org/copyright">OpenStreetMap</a>contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {markers.map((marker) => (
+        <Marker position={marker.geocode}></Marker>
+      ))}
     </MapContainer>
   );
 }
